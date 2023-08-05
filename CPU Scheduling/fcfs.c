560eff30d90ff6545405691bbfa7a8fb42ec4acb
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-08-05 13:07:34 +0530

    Update fcfs.c

--- CPU Scheduling/fcfs.c
+++ CPU Scheduling/fcfs.c
@@ -2,33 +2,40 @@
 #include<stdio.h>
 struct fcfs{
     int pid;
+    int atime;
     int btime;
+    int ctime;
     int wtime;
-    int ttime;
+    int tatime;
 }p[10];
 
-int main(){
+void main(){
     int i,n;
     int totwtime=0,totttime=0;
-    printf("-------FCFS SCHEDULING---------");
-    printf("enter the number of processes:";
+    printf("-------FCFS SCHEDULING---------\n");
+    printf("enter the number of processes:");
     scanf("%d",&n);
-    for(i=0;i<n;i++){
-        p[i].pid=1;
+    for(i=1;i<=n;i++){
+        p[i].pid=i;
         printf("enter the bursttime:\n");
-        printf("btime of %d:\n",p[i]);
+        printf("btime of %d:\n",p[i].pid);
         scanf("%d",&p[i].btime);
+        printf("enter the arrivaltime:\n");
+        printf("atime of %d:\n",p[i].pid);
+        scanf("%d",&p[i].atime);
     }
     p[0].wtime=0;
-    p[0].ttime=p[0].btime;
-    for(i=0;i<n;i++){
-        p[i].wtime=p[i-1].wtime + p[i-1]btime;
-        p[i].ttime=p[i].wtime+p[i].btime;
-        totttime+=p[i].ttime;
-        totwtime+=p[i].wtime;
-    }
-    printf("Total Waiting Time:%d\n",totwtime);
-    printf("Average Waiting Time:%d\n",totwtime/n);
-    printf("Total Turn Around Time:%d\n",totttime);
-    printf("Average Turn Around Time:%d\n",totttime/n);    
-}
+    p[0].ctime=0;
+    for(i=1;i<=n;i++){
+     p[i].ctime=p[i].btime+p[i-1].ctime;
+     p[i].tatime=p[i].ctime-p[i].atime;
+     totttime+=p[i].tatime;
+     p[i].wtime=p[i].tatime-p[i].btime;
+     totwtime+=p[i].wtime;}
+     
+     printf("-------------------TABLE-------------------");
+     printf("\npid\tatime \tbtime \tctime \ttat \twt \t \n");
+      for(i=1;i<=n;i++){
+          printf("\n %d \t %d \t %d \t %d \t %d \t %d \t \n",p[i].pid,p[i].atime,p[i].btime,p[i].ctime,p[i].tatime,p[i].wtime);}
+          
+          printf("\nAverage TurnAroundTime= %d \nAverage WaitingTime=%d \n",totttime/n,totwtime/n);}
